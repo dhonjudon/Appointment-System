c7e3b12e7c63d70d9c32fb850b134f7848366a97
unknown <snehasah2216@gmail.com>
2026-05-19 13:49:53 +0545

    fix: resolve App.jsx duplicate code and install date-fns

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,3 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Navbar from './pages/Navbar';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Dashboard from './pages/Dashboard';
-import AppointmentBooked from './pages/AppointmentBooked';
-import Feedback from './pages/Feedback';
-import DoctorDashboard from './doctor-pages/DoctorDashboard';
-import Patients from './doctor-pages/Patients';
-import DoctorReports from './doctor-pages/DoctorReports';
-import DoctorProfileSetup from './doctor-pages/DoctorProfileSetup';
-
-function App() {
-  return (
-    <BrowserRouter>
-      <AppContent />
 import React from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./pages/Navbar";
@@ -30,10 +13,10 @@ import BookAppointment from "./pages/BookAppointment";
 import AppointmentSetup from "./pages/doctor/AppointmentDetails";
 import DoctorDashboard from "./pages/doctor/dashboard";
 import DoctorAppointments from "./pages/doctor/Appointments";
+
 function App() {
   return (
     <BrowserRouter>
-      {/* <Navbar />  */}
       <div className="bg-gray-50 min-h-screen">
         <Routes>
           <Route path="/" element={<Navigate to="/login" />} />
